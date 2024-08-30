98d832ef8edb137d26a8317f607479461b25b570
AbdillahNS <abdillahnoersaid@gmail.com>
2024-08-30 14:21:53 +0700

    Percobaan 1

---
+++ Pertemuan 1/SepedaDemo01.java
@@ -0,0 +1,18 @@
+public class SepedaDemo01 {
+    public static void main(String[] args) {
+        Sepeda01 spd1 = new Sepeda01();
+        Sepeda01 spd2 = new Sepeda01();
+
+        spd1.setMerek("Polygone");
+        spd1.tambahKecepatan(10);
+        spd1.gantiGear(2);
+        spd1.cetakStatus();
+
+        spd2.setMerek("Wiim Cycle");
+        spd2.tambahKecepatan(10);
+        spd2.gantiGear(2);
+        spd2.tambahKecepatan(10);
+        spd2.gantiGear(3);
+        spd2.cetakStatus();
+    }
+}
